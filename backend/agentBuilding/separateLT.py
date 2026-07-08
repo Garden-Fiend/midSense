71bc4a434e4b6dade8d20fe90eb05a384e0efe8b
Garden Fiend <rainethegardenfiend@gmail.com>
2026-07-08 23:34:14 +0800

    trying to understand dictionary behavior

--- backend/agentBuilding/separateLT.py
+++ backend/agentBuilding/separateLT.py
@@ -8,6 +8,7 @@ officePc = "Microsoft Wi-Fi Direct Virtual Adapter #2"
 print("Observation Starting")
 
 deviceTable = {}
+lookUpTable = {}
 
 
 def observe(pkt):
@@ -31,19 +32,34 @@ def observe(pkt):
             "Uploads":0,
             "Downloads":0
         }
+        
+        lookUpTable[ipSrc] = macSrc
 
     deviceTable[macSrc]["IpAddress"] = ipSrc
-
-    if (ipSrc == deviceTable[macSrc]["IpAddress"]):
-        deviceTable[macSrc]["Uploads"] += len(pkt)
     
-    elif(ipDst == deviceTable[macSrc]["IpAddress"]):
-        deviceTable[macSrc]["Downloads"] += len(pkt)
+    
+    if(ipSrc in lookUpTable):
+        print(lookUpTable[ipSrc])
+        deviceTable[lookUpTable[ipSrc]]["Uploads"] += len(pkt)
+    
+    if(ipDst in lookUpTable):
+        print(lookUpTable[ipDst])
+        deviceTable[lookUpTable[ipDst]]["Uploads"] += len(pkt)
+        
 
-sniff(iface=homePc,timeout=60,prn=observe)
+    
 
-print(deviceTable)
+sniff(iface=homePc,timeout=20,prn=observe)
 
+for device in deviceTable:
+    print("-------------------------")
+    print(device)
+    print(deviceTable[device]["IpAddress"])
+    print(deviceTable[device]["Uploads"])
+    print(deviceTable[device]["Downloads"])
+    
+
+    
     
 
     
